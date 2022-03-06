53460259f840778eeb4b8bcc9c9443e4f07e3fbf
Yasir Hassan <yasir.hassan1993@hotmail.com>
2022-03-06 23:00:05 +0500

    Secret Key change

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY = 'Keyboard_KAT'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
